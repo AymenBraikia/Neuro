bc427292754e694da8fc62d059029858b616ed92
AymenBraikia <aymenbraikia1@gmail.com>
2025-05-25 13:37:42 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -109,7 +109,7 @@ app.post("/signin", async (req, res) => {
 		return;
 	}
 
-	res.cookie("username", data.username, { httpOnly: true, secure: true, sameSite: "none" }).send(`<script>window.location.href = "${req.headers.origin || "http://localhost:3000/"}"</script>`);
+	res.cookie("username", data.username, { httpOnly: true, secure: true, sameSite: "none" }).send(`<script>alert(5);window.location.href = "${req.headers.origin || "http://localhost:3000/"}"</script>`);
 });
 
 app.listen(port, () => {
